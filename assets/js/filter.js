a30d3d17a426bc78e9437feb4a82ddf19a666948
tquinonero <152458286+tquinonero@users.noreply.github.com>
2024-09-18 00:57:30 +0200

    Update filter.js
    Add AJAX

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,1 +1,21 @@
-#
+jQuery(document).ready(function($) {
+    console.log('WC Category Filter script loaded');
+    $('#wc-category-filter').on('submit', function(e) {
+        e.preventDefault();
+        var category = $('#product_cat').val();
+
+        $.ajax({
+            url: wc_filter_params.ajax_url,
+            type: 'POST',
+            data: {
+                action: 'wc_category_filter',
+                category: category
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#wc-category-filter-results').html(response.data);
+                }
+            }
+        });
+    });
+});
